10e3d33f30c262c8cdd955db367ae4e35515bc5d
Patryk Dagiel <patryk.dagiel@gmail.com>
2020-04-26 21:27:59 +0200

    Second part

--- LoonyLips/LonnyLips.gd
+++ LoonyLips/LonnyLips.gd
@@ -1,13 +1,43 @@
 extends Control
 
+var player_words = []
+var story = "Once upon a time there was %s in %s and %s in %s"
+var prompts = ["a name", "a noun", "an adverb", "an adjective"]
+
+onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
+onready var DisplayText = $VBoxContainer/DisplayText
+
 func _ready():
-	var prompts = ["Yann", "Minions", "Greatest"]
-	var story = "Once upon a time there was %s in %s and %s"
-	$VBoxContainer/DisplayText.text = (story % prompts)
-	
+	welcome_prompt()
+	check_player_words_length()
+
+
 func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
+	add_to_player_words()
+
+func _on_TextureButton_pressed():
+	add_to_player_words()
 	
-func update_DisplayText(words):
-	$VBoxContainer/DisplayText.text = words 
-	$VBoxContainer/PlayerText.clear()
+func welcome_prompt():
+	DisplayText.text = "Welcome to the game"
+	
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	DisplayText.text = ""
+	PlayerText.clear()
+	check_player_words_length()
+
+func is_story_done(): 
+	return player_words.size() == prompts.size()
+	
+func prompt_player():
+	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
+
+func tell_story():
+	DisplayText.text = story % player_words
+
+func check_player_words_length():
+	if is_story_done():
+		tell_story()
+	else:
+		prompt_player()
