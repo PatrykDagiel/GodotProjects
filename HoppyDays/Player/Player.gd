271f0bba3554bb816396c262931f3e6bd13f263f
-l <patryk.dagiel@gmail.com>
2020-05-04 19:35:50 +0200

    First Part HD v2

--- HoppyDays/Player/Player.gd
+++ HoppyDays/Player/Player.gd
@@ -2,15 +2,16 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 
-const SPEED = 1000
+const SPEED = 1500
 const GRAVITY = 300
 const UP = Vector2(0,-1)
-const JUMP_SPEED = 3000
+const JUMP_SPEED = 5000
 
 func _physics_process(delta):
 	apply_gravity()
 	jump()
 	move()
+	animate()
 	move_and_slide(motion, UP)
 
 func apply_gravity():
@@ -19,7 +20,7 @@ func apply_gravity():
 	else:
 		motion.y += GRAVITY
 func jump():
-	if (Input.is_action_just_pressed("jump") and is_on_floor()):
+	if (Input.is_action_pressed("jump") and is_on_floor()):
 		motion.y -= JUMP_SPEED
 
 func move():
@@ -29,3 +30,15 @@ func move():
 		motion.x = SPEED
 	else:
 		motion.x = 0
+		
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x > 0:
+		$AnimatedSprite.set_flip_h(false)
+		$AnimatedSprite.play("walk")
+	elif motion.x < 0:
+		$AnimatedSprite.set_flip_h(true)
+		$AnimatedSprite.play("walk")
+	else:
+		$AnimatedSprite.play("idle")
